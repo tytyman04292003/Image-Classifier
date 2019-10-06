ce388766c639cae494bcdc183cf1fb27f5839c24
Tyman Huang <tman160429@gmail.com>
2019-10-06 10:50:10 +0800

    Updated based on reviewer

--- helpers/LoadJson.py
+++ helpers/LoadJson.py
@@ -1,7 +1,7 @@
 import json
 
 def load_json(filepath):
-    with open('cat_to_name.json', 'r') as f:
+    with open(filepath, 'r') as f:
         cat_to_name = json.load(f)
         
     return cat_to_name
